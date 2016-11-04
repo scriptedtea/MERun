0e15f02a2e39f4e9fc693c4abf29c7b4c469d5e3
Bryan Tai <redhatpieman@gmail.com>
2016-11-03 20:14:39 -0700

    Added recovery logic... but i'm not sure how to apply it

--- Assets/Scripts/MERun.cs
+++ Assets/Scripts/MERun.cs
@@ -8,18 +8,29 @@ public class MERun : MonoBehaviour {
 	const int FLOOR_SPAWN_TIME = 100;
 
 
+
 	public EnvironmentSpawner environmentSpawner;
 	public Text timerText;
+	private static int recoverTimer = 500;
 	public static float worldSpeed = 0.5f;
 
 	// Use this for initialization
 	void Start () {
 		//Camera.main.projectionMatrix = Matrix4x4.Ortho (-5.0f * 1.6f, 5.0f * 1.6f, -5.0f, 0.5f, 0.3f, 1000f);
 		timer = 0;
+
+		environmentSpawner.spawnBuilding ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (worldSpeed < 0.5f && recoverTimer % 100 == 0) {
+			worldSpeed += 0.1f;
+		}
+		if (recoverTimer < 500) {
+			recoverTimer++;
+		}
 
 		//TODO make this per second
 		timerText.text = timer.ToString();
@@ -40,8 +51,11 @@ public class MERun : MonoBehaviour {
 		if (timer > 1000000) {
 			timer = 0;
 		}
+	}
 
-
-	
+	//TODO How do we determine if player has collided with an obstacle?
+	public static void playerCrashed(){
+		worldSpeed = 0;
+		recoverTimer = 0;
 	}
 }
