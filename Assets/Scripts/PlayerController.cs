7e701b675db73fee37b95ce003a5664aa0c7b1f1
Lily Guo <lily2guo@gmail.com>
2016-11-04 14:49:31 -0700

    Implemented sliding isn, added animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,8 @@ public class PlayerController : MonoBehaviour {
 	public Transform groundCheck;
 	public Transform obstacleCheck;
 
-	float groundRadius = 0.2f;
-	float obstacleRadius = 0.2f;
+	float GROUND_RADIUS = 0.2f;
+	float OBSTACLE_RADIUS = 0.2f;
 
 	bool grounded = false;
 	bool hasObstacle = false;
@@ -15,36 +15,48 @@ public class PlayerController : MonoBehaviour {
 	public LayerMask whatIsGround;
 	public LayerMask whatIsObstacle;
 
-	float maxJumpForce = 700f;
+	float MAX_JUMP_FORCE = 700f;
 	float jumpForce = 400f;
 	Rigidbody2D rb;
 
+	Animator anim;
+
 	bool touchLifted = false;
+	bool isSlidingnow = false;
+
+	float slidingDuration = 0f;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
-		hasObstacle = Physics2D.OverlapCircle (obstacleCheck.position, obstacleRadius, whatIsObstacle);
+		grounded = Physics2D.OverlapCircle (groundCheck.position, GROUND_RADIUS, whatIsGround);
+		hasObstacle = Physics2D.OverlapCircle (obstacleCheck.position, OBSTACLE_RADIUS, whatIsObstacle);
 	}
 
 	void Update() {
-		if (grounded) {
-			if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) {
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) {
+			if (grounded && !isSlidingnow) {
 				rb.AddForce (new Vector2 (0, jumpForce));
 				touchLifted = false;
-			} 
+			}
 		} else if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Stationary) {
 			//float currentForce = rb.velocity.magnitude;
-			if (jumpForce < maxJumpForce && !touchLifted) {
+			if (jumpForce < MAX_JUMP_FORCE && !touchLifted) {
 				float multiplier = jumpForce / 100f * 20f;
 				rb.AddForce (new Vector2 (0, multiplier));
 				jumpForce = jumpForce + 100;
 				touchLifted = false;
+			}
+		} else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+			// Get movement of the finger since last frame
+			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+			if (touchDeltaPosition.y < -1) {
+				slide ();
 			}
 		} else if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Ended) {
 			jumpForce = 400f;
@@ -53,6 +65,28 @@ public class PlayerController : MonoBehaviour {
 
 		if (hasObstacle) {
 			MERun.playerCrashed ();
+		}
+			
+		checkSlide ();
+	}
+
+	void slide() {
+		if (!isSlidingnow) {
+			anim.SetBool ("isSliding", true);
+			isSlidingnow = true;
+		}
+	}
+
+	void checkSlide() {
+		if (isSlidingnow) {
+			if (slidingDuration > 20) {
+				isSlidingnow = false;
+				anim.SetBool ("isSliding", false);
+				slidingDuration = 0;
+				return;
+			} else {
+				slidingDuration++;
+			}
 		}
 	}
 }
