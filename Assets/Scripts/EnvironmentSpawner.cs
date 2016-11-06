e8f9b5d1ef700e5e945d2e75e205186c5796b6fa
Bryan Tai <redhatpieman@gmail.com>
2016-11-06 15:22:54 -0800

    Added redbox, added layering to sprites, adjusted level generation

--- Assets/Scripts/EnvironmentSpawner.cs
+++ Assets/Scripts/EnvironmentSpawner.cs
@@ -8,9 +8,12 @@ public class EnvironmentSpawner : MonoBehaviour {
 	public static Box boxPrefab;
 	public static RaisedBoxesBuilding raisedBoxesPrefab;
 	public static SlideBox slideBoxPrefab;
+	public static RedBox redBoxPrefab;
 
 	//TODO refactor back to int?
 	private const float SPAWN_OFFSET_X = 75;
+
+	private static bool nextBuildingIsAssisted = false;
 
 	// Use this for initialization
 	void Start () {
@@ -20,43 +23,92 @@ public class EnvironmentSpawner : MonoBehaviour {
 
 		boxPrefab 			= (Box)Resources.Load ("Prefabs/Box",  typeof(Box));
 		slideBoxPrefab 		= (SlideBox)Resources.Load ("Prefabs/SlideBox", typeof(SlideBox));
+		redBoxPrefab 		= (RedBox)Resources.Load ("Prefabs/RedBox", typeof(RedBox));
 	}
 
+
+
 	public static void spawnEnvironment(){
-		int rand = Random.Range (1, 3);
-		if (rand > 1) {
-			spawnBuilding ();
-		} else {
-			spawnObstacle ();
+		int whichObstacle = Random.Range (0, 3);
+		int whichBuilding = Random.Range (0, 2);
+		int whichAid = Random.Range (0, 2);
+
+		switch (whichObstacle) {
+		case 1:
+			spawnBox ();
+			break;
+		case 2:
+			spawnSlideBox ();
+			spawnPlainBuilding ();
+			return;
+		}
+
+		switch (whichBuilding) {
+		case 0:
+			spawnPlainBuilding ();
+			break;
+		case 1:
+			spawnRaisedBoxesBuilding ();
+			break;
+		}
+
+
+		switch (whichAid) {
+		case 1:
+			spawnDoubleAid ();
+			break;
 		}
 	}
+
+
+	private static void spawnAid(){
+		int redBoxOffset = 25;
+		redBoxPrefab.Spawn (new Vector3 (SPAWN_OFFSET_X + redBoxOffset, -1.75f));
+		nextBuildingIsAssisted = true;
+	}
+	private static void spawnDoubleAid(){
+		int redBoxOffset = 15;
+		redBoxPrefab.Spawn (new Vector3 (SPAWN_OFFSET_X + redBoxOffset, -1.75f));
+		spawnAid ();
+
+	}
+
 	
 	private static void spawnObstacle(){
 
 		int rand = Random.Range (0, 2);
 		if (rand > 0) {
-			int boxOffset = -1;
-			boxPrefab.Spawn(new Vector3 (SPAWN_OFFSET_X + boxOffset, -1.5f));
+			spawnBox ();
 			spawnPlainBuilding ();
 		} else {
-
-			slideBoxPrefab.Spawn (new Vector3 (SPAWN_OFFSET_X, 3));
+			spawnSlideBox ();
 			spawnPlainBuilding ();
 		}
+	}
 
+	private static void spawnBox(){
+		int boxOffset = -1;
+		boxPrefab.Spawn(new Vector3 (SPAWN_OFFSET_X + boxOffset, -1.5f));
+	}
 
+	private static void spawnSlideBox(){
+		slideBoxPrefab.Spawn (new Vector3 (SPAWN_OFFSET_X, 3));
 	}
 
 	public static void spawnBuilding(){
-		int rand = Random.Range (0, 4);
+		int rand = 1; //Random.Range (0, 2);
 		if (rand > 0) {
 			spawnPlainBuilding ();
 		} else {
-			raisedBoxesPrefab.Spawn (new Vector3 (SPAWN_OFFSET_X, 2f));
+			spawnRaisedBoxesBuilding ();
 		}
 	}
 
 	private static void spawnPlainBuilding(){
 		buildingPrefab.Spawn (new Vector3 (SPAWN_OFFSET_X, -8f));
+	}
+
+	private static void spawnRaisedBoxesBuilding(){
+		raisedBoxesPrefab.Spawn (new Vector3 (SPAWN_OFFSET_X, 2f));
 	}
 }
