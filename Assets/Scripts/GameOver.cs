92dc1a9073b73593d485dae2d24f13d6ece8e937
Bryan Tai <redhatpieman@gmail.com>
2016-11-06 15:40:57 -0800

    Added retry button functionality to GameOver screen

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	public void restartGame(){
+		Debug.Log ("RESTART!");
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Start");
+	}
+}
